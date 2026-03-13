89fdf482d7b4f728ed5f364bd9a50c73e8f16c3a
AndrDemchuk <xdemchuk@gmail.com>
2026-03-13 23:06:21 +0200

    add book success/failure actions & reducers created, as well as an effect for them

--- src/app/books/book.reducer.ts
+++ src/app/books/book.reducer.ts
@@ -1,11 +1,17 @@
 import { createReducer, on } from '@ngrx/store';
-import { AddBook, RemoveBook } from './book.actions';
+import { AddBook, RemoveBook, AddBookSuccess, AddBookFailure } from './book.actions';
 import { Book } from '../models/book';
 
 export const initialState: Book[] = [];
 export const BookReducer = createReducer(
   initialState,
-  on(AddBook, (state, { id, title, author }) => [...state, { id, title, author }]),
+  on(AddBook, (state) => { return state }),
+  on(AddBookSuccess, (state, { id, title, author }) => [...state, { id, title, author }]),
+  on(AddBookFailure, (state, { error }) => {
+    console.error(error);
+    return state;
+  }),
+  
   //we`ve gonna add a new book to the state, we`ll return a new array with the spread operator and add the new book to it
   on(RemoveBook, (state, { bookId }) => state.filter((book) => book.id !== bookId)),
   //we`ll filter out the bookId book out of the array and return a new array without it
