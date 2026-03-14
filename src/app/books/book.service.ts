ea9e929c15757b914f6e202607ffc06eb4e6ec78
AndrDemchuk <xdemchuk@gmail.com>
2026-03-14 20:10:15 +0200

    devtools ngrx testing extensions added

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -10,6 +10,9 @@ export class BookService {
     constructor() { }
 
     addBook(book: Book): Observable<Book> {
+        //uncomment to invoke an error
+        /*         const err = new Error('err while adding a book ')
+                return throwError(() => err) */
         return of(book);
     }
 }
